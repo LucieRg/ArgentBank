169c13e41bc2056ec0bad210bfc331d560ce5262
Lucie Rogé <roge.lucie@gmail.com>
2023-09-06 13:50:27 +0200

    Home page

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../../public/img/argentBankLogo.png";
+
+const Nav = () => {
+  return (
+    <nav className="main-nav">
+      <NavLink className="main-nav-logo" to="/">
+        <img
+          className="main-nav-logo-image"
+          src={Logo}
+          alt="Logo Argent Bank"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </NavLink>
+      <div>
+        <NavLink className="main-nav-item" to="/login">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
